a0c4c74c362faa4adfa59c76c9a5d989754594b3
jurichmitchell <jurichmitchell@gmail.com>
2024-02-15 19:34:11 -0600

    console-window component styling and functionality adjustments
    The console-window component now can take in 2 inputs: a string for the Title and a string for the window's innerHTML.
    
    The second value is treated as innerHTML so that document markup (such as <p> tags) can be utilized inside the window section of the component.
    
    In order for stylings to be applied to that innerHTML, the console-window component now utilizes a ViewEncapsulation of ShadowDom. This allows for HTML generated outside the component itself to be styled according to the component's stylings.
    
    Additionally, the string value passed into the component for the window's innerHTML is sanitized using DomSanitizer.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,4 +14,7 @@ import { ConsoleWindowComponent } from './console-window/console-window.componen
 })
 export class AppComponent {
   title = 'angular-app-anatomy';
+
+	consoleWindowTitle: string = "Creating a Component via Angular CLI";
+	consoleWindowText: string = "<p>LINE ONE</p><p>LINE TWO</p><p>LINE THREE</p>";
 }
--- src/app/console-window/console-window.component.ts
+++ src/app/console-window/console-window.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-console-window',
@@ -8,18 +9,29 @@ import { Component } from '@angular/core';
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,wght@0,200..900;1,200..900&display=swap" rel="stylesheet">
 		<div class="console-window">
 			<div class="titlebar">
-				<div class="title">Creating a Component via Angular CLI</div>
+				<div class="title">{{ titleTxt }}</div>
 				<div class="icons">
 					<div class="minimize-ico"><span>&#9135;</span></div>
 					<div class="maximize-ico"><span>&#9634;</span></div>
 					<div class="close-ico"><span>&#10005;</span></div>
 				</div>
 			</div>
-			<div class="window-text">Here is some test text for a console command or whatever</div>
+			<div class="window-text" [innerHTML]=windowTxt></div>
 		</div>
   `,
-  styleUrl: './console-window.component.css'
+  styleUrl: './console-window.component.css',
+	encapsulation: ViewEncapsulation.ShadowDom
 })
 export class ConsoleWindowComponent {
+	@Input() titleTxt!: string;
+	@Input() windowTxt!: string;
+	sanitizer: DomSanitizer;
 
+	constructor(sanitizer: DomSanitizer) {
+		this.sanitizer = sanitizer;
+	}
+
+	sanitize(html: string) {
+		return this.sanitizer.bypassSecurityTrustHtml(html);
+	}
 }
